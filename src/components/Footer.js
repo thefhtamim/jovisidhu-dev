9fdd88727b923225aa8d1938c5bf9a5b48200211
Jovi <jovimdl@gmail.com>
2020-09-21 15:26:21 -0700

    Footer Rework
    Redid how the footer is centered, and simplified the Footer.js code by cutting out all the Grid and Paper nonsense from when I was trying to style it when the site was first made, code is much cleaner.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,30 +2,18 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { GitHub, LinkedIn, Instagram }  from '@material-ui/icons';
 import styles from './Footer.module.css';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
+import {Toolbar, Box } from '@material-ui/core';
 
 
 const useStyles = makeStyles(theme => ({
     root: {
         flexGrow: 1,
-        marginLeft: 15,
-        marginBottom: 10,
-        marginTop: 10,
         top: 'auto',
-        bottom: 0,
+        bottom: 40,
         width: "100%",
         background: '#FFFFFF',
         position: 'fixed',
         height: "150",
-    },
-    paper: {
-        height: 100,
-        width: 100,
-        direction: "column",
-        alignItems: "center",
-        justify: "center",
-        backgroundColor: "#FFFFFF",
     },
 }));
 
@@ -35,27 +23,13 @@ function Footer() {
 
     return(
         <div display="block">
-            <Grid container className={classes.root} spacing={2}>
-                <Grid item xs={"10px"}>
-                    <Grid container  spacing={1} className={styles.leftAlign}>
-                        <Grid key={1} item xs>
-                            <Paper className={classes.paper} elevation={0}>
-                                <GitHub className={styles.githubIcon} onClick={() => window.open("https://github.com/Mal-Jovi/", " _blank")}></GitHub>
-                            </Paper>
-                        </Grid>
-                        <Grid key={2} item xs>
-                            <Paper className={classes.paper} elevation={0}>
-                                <LinkedIn className={styles.infoIcon} onClick={() => window.open("https://www.linkedin.com/in/jovi-sidhu/", " _blank")}></LinkedIn>
-                            </Paper>
-                        </Grid>
-                        <Grid key={3} item xs>
-                            <Paper className={classes.paper} elevation={0}>
-                                <Instagram className={styles.infoIcon} onClick={() => window.open("https://www.instagram.com/maljovi/", " _blank")}></Instagram>
-                            </Paper>
-                        </Grid>
-                    </Grid>
-                </Grid>
-            </Grid>
+            <Toolbar className={classes.root}>
+                <Box style={{flex: 1}}></Box>
+                <GitHub className={styles.infoIcon} onClick={() => window.open("https://github.com/Mal-Jovi/", " _blank")}></GitHub>
+                <LinkedIn className={styles.infoIcon} onClick={() => window.open("https://www.linkedin.com/in/jovi-sidhu/", " _blank")}></LinkedIn>
+                <Instagram className={styles.infoIcon} onClick={() => window.open("https://www.instagram.com/maljovi/", " _blank")}></Instagram>
+                <Box style={{flex: 1}}></Box>
+            </Toolbar>
         </div>
     );
 }
