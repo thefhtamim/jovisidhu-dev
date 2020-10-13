9ae112f088dd5e86a781e0b09afdff20e3570e2f
Jovi <jovimdl@gmail.com>
2020-10-13 00:21:47 -0700

    jovisidhu-dev work page draft
    Did a  lot of playing around with how to organize an arrange elements for a given portfolio component.
    
    Still need to nail down how to resize the container to fit the needs of the content. If I remove the border and set the overflow to override the container's bounds then it may work, but may cause problem with bottom margins, which can be further ignored by keep all portfolio content in the hostContainer. Still might runover onto the footer, will test out later.
    
    Text boxes with images interspersed on teh left and right seems interesting, but just having elements stacking in a flex column might be most convenient.

--- src/pages/jovisidhudev.js
+++ src/pages/jovisidhudev.js
@@ -2,27 +2,56 @@ import React from 'react';
 import { motion } from "framer-motion";
 import { Typography } from '@material-ui/core';
 import { makeStyles, ThemeProvider, createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
+import image from '../assets/artboard1.svg';
  
 
 const useStyles = makeStyles((theme) => ({
   containerElement: {
     margin: "5px",
-    width: "250px",
-    height: "150px",
+    width: "63vw",
+    height: "40vh",
+    
     marginRight: "65px",
+
+    border: "solid",
+    borderColor: "black",
+    borderRadius: "20px",
+    borderWidth: "5px",
+
+    backgroundColor: "transparent",
+
+    overflow: "hidden",
+    [theme.breakpoints.down('xs')]: {
+      width: "90vw",
+      height: "60vh",
+      },
     },
     elementText: {
-      paddingTop: "100px",
+      paddingTop: "10px",
       paddingLeft: "15px",
-      color: "white",
+      paddingRight: "15px",
+      color: "black",
       margin: "auto",
+      overflow: "wrap",
     },
     hostContainer: {
       justifyContent: "center",
       alignItems: "center", 
       display: "flex",
       flexFlow: "column wrap",
+      marginTop: "50px",
     },
+    innerContainer: {
+      justifyContent: "flex-start",
+    },
+    subtitle: {
+      marginLeft: "15px",
+      fontSize: "20px",
+    },
+    menuIcon: {
+      marginLeft: "15px",
+      marginTop: "25px",
+    }
 }));
 
 let mainTheme = createMuiTheme({
@@ -43,24 +72,37 @@ function Jovisidhudev() {
 
   const classes = useStyles();
 
-  const hover = {
-    variantA: { scale: 1.5 },
-    variantB: { scale: 5 },
+  const inner = {
+    entry: { transition:{duration: 0.9}, opacity: 1 },
   }
 
     return (
       <div className={classes.hostContainer}>
-        <h2>jovisidhudev</h2>
-        <motion.div
-          className={classes.containerElement}
-          variants={hover}
-          whileHover={"variantA"}
-          size={150}
-          radius={30}
-          style={{backgroundColor: "#000000"}}
-        >
-          <Typography className={classes.elementText}>This page is currently underconstruction! I'm probably working on it right now!</Typography>
-        </motion.div>
+        <div className={classes.innerContainer}>
+          <Typography className={classes.subtitle}>jovisidhudev</Typography>
+          <motion.div
+            className={classes.containerElement}
+            variants={inner}
+            initial={{opacity: 0}}
+            animate={"entry"}
+            size={150}
+            radius={30}
+          >
+            <Typography className={classes.elementText}>
+              This page is currently underconstruction! I'm probably working on it right now!
+              <br></br>
+              <br></br>
+              This focus of this project was to create a website that displayed my understanding of efficient design layouts and modern
+              front-end development design principles.
+
+              The site had to be easy to navigate, visually appealling, mobile friendly, and had to be built using a modern front end frameworks and web development packages.
+              <br></br>
+              <br></br>
+              The site is a React.js aplication made using HTML, CSS, Javascript, react-router for dynamically loading in components, material-UI for quickly setting up capable components, and framer-motion for the animations.
+            </Typography>
+            <img className={classes.menuIcon} src={image} alt="jovi_logo" height={100} width={100} />
+          </motion.div>
+        </div>
       </div>
     );
 }
